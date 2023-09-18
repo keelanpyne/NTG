4236e027322673913c4fa5d0043addb8e72bcd3c
keelanpyne <144889919+keelanpyne@users.noreply.github.com>
2023-09-18 13:06:26 +0200

    Update cleaned.Jobs (in progress).sql

--- cleaned.Jobs (in progress).sql
+++ cleaned.Jobs (in progress).sql
@@ -1,32 +1,3 @@
------ AP Rev Recognition ------
-
-WITH AP AS (
-    SELECT
-        D.AL_PostDate as "AL_AP_PostDate",
-        D.AL_PK,
-        D.AL_RevRecognitionType as "AL_AP_RevRecognitionType",
-        E.JR_JH
-    FROM cargowise_dk_wrld.AccTransactionLines as D
-    JOIN cargowise_dk_wrld.JobCharge as E
-    ON D.AL_PK = E.JR_AL_APLine
-),
-
--- SELECT * FROM AP;
-
-AP_JR AS (
-    SELECT
-        D3_JH, 
-        D3_RecognitionType
-    FROM cargowise_dk_wrld.JobChargeRevRecognition)
-
--- SELECT * FROM AP_JR;
-    
-SELECT 
-    * 
-FROM AP_JR
-LEFT OUTER JOIN AP
-ON AP.JR_JH = AP_JR.D3_JH AND AP.AL_AP_RevRecognitionType = AP_JR.D3_RecognitionType;
-
 ----- AR Rev Recognition ------
 
 WITH AR AS (
@@ -34,24 +5,61 @@ WITH AR AS (
         D.AL_PostDate as "AL_AR_PostDate",
         D.AL_PK,
         D.AL_RevRecognitionType as "AL_AR_RevRecognitionType",
+        E.JR_PK,
+        E.JR_GE,
+        E.JR_GB,
+        E.JR_AC,
+        E.JR_OH_CostAccount,
+        E.JR_LocalCostAmt,
+        E.JR_PaymentDate,
+        E.JR_OH_SellAccount,
+        E.JR_LocalSellAmt,
+        E.JR_ChargeType,
+        E.JR_EstimatedCost,
+        E.JR_EstimatedRevenue,
         E.JR_JH
     FROM cargowise_dk_wrld.AccTransactionLines as D
     JOIN cargowise_dk_wrld.JobCharge as E
     ON D.AL_PK = E.JR_AL_ARLine
 ),
 
--- SELECT * FROM AP;
-
 AR_JR AS (
     SELECT
         D3_JH, 
-        D3_RecognitionType
+        D3_RecognitionType,
+        D3_RecognitionDate AS "AR_RecognitionDate"
     FROM cargowise_dk_wrld.JobChargeRevRecognition)
 
--- SELECT * FROM AP_JR;
-    
 SELECT 
-   top 10 * 
+       *,
+       CASE 
+            WHEN AR_RecognitionDate = '1900-01-01 00:00:00' THEN NULL
+            ELSE AR_RecognitionDate
+        END AS AR_RecognitionDate,
+        CASE
+            WHEN AL_AR_RevRecognitionType = 'IMM' THEN
+               CASE
+                   WHEN AL_AR_PostDate IS NULL THEN AL_AR_PostDate
+                   ELSE AL_AR_PostDate
+               END
+           ELSE AR_RecognitionDate
+       END AS RevenueRecognitionDate,
+       CASE
+           WHEN AL_AR_RevRecognitionType = 'IMM' THEN
+               CASE
+                   WHEN AL_AR_PostDate IS NULL THEN AL_AR_PostDate
+                   ELSE AL_AR_PostDate
+               END
+           ELSE AR_RecognitionDate
+       END AS CostRecognitionDate
 FROM AR_JR
 LEFT OUTER JOIN AR
 ON AR.JR_JH = AR_JR.D3_JH AND AR.AL_AR_RevRecognitionType = AR_JR.D3_RecognitionType;
+
+
+
+
+
+
+
+
