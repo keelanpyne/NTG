2c286b6a9b575e24dfa877b68ca88977ba75e6aa
keelanpyne <144889919+keelanpyne@users.noreply.github.com>
2023-09-18 14:17:43 +0200

    Update cleaned.Jobs (in progress).sql

--- cleaned.Jobs (in progress).sql
+++ cleaned.Jobs (in progress).sql
@@ -28,7 +28,49 @@ AR_JR AS (
         D3_JH, 
         D3_RecognitionType,
         D3_RecognitionDate AS "AR_RecognitionDate"
-    FROM cargowise_dk_wrld.JobChargeRevRecognition)
+    FROM cargowise_dk_wrld.JobChargeRevRecognition),
+
+AR_JH AS(
+    SELECT
+        F.JH_JobNum,
+        F.JH_Status,
+        F.JH_A_JOP,
+        F.JH_A_JCL,
+        F.JH_OA_LocalChargesAddr,
+        F.JH_OA_AgentCollectAddr,
+        F.JH_GS_NKRepSales,
+        F.JH_GS_NKRepOps,
+        F.JH_JobType,
+        F.JE,
+       --F.JK,
+       --F.WD,
+        F.WKI,
+        F.JS,
+        F.TH
+        --F.BF
+    FROM cargowise_dk_wrld.JobHeader AS F
+    JOIN cargowise_dk_wrld.JobCharge AS G
+    ON F.JH_PK = G.JR_JH
+),
+
+AR_JS AS(
+    SELECT
+        H.JS_ShipmentStatus,
+        H.JS_RL_NKOrigin,
+        H.JS_RL_NKDestination,
+        H.JS_TransportMode,
+        H.JS_ActualVolume,
+        H.JS_UnitOfVolume,
+        H.JS_ActualWeight,
+        H.JS_UnitOfWeight,
+        H.JS_ActualChargeable,
+        H.JS_UnitFreightRate,
+        H.JS_ShipmentType,
+        H.JS_LoadingMeters,
+        H.JS_RL_NKPlaceOfReceipt,
+        H.JS_RL_NKPlaceOfDischarge
+    FROM cargowise_dk_wrld.JobShipment AS H
+)
 
 SELECT 
        *,
@@ -55,6 +97,8 @@ SELECT
 FROM AR_JR
 LEFT OUTER JOIN AR
 ON AR.JR_JH = AR_JR.D3_JH AND AR.AL_AR_RevRecognitionType = AR_JR.D3_RecognitionType;
+
+SELECT TOP 1 * FROM cargowise_dk_wrld.JobHeader;
 
 
 
