87f505af601f69ef6272523dd978703d4b25597b
keelanpyne <144889919+keelanpyne@users.noreply.github.com>
2023-09-21 14:25:51 +0200

    Update analytics.OrgHeader.sql

--- analytics.OrgHeader.sql
+++ analytics.OrgHeader.sql
@@ -1,7 +1,8 @@
-CREATE VIEW analytics.OrgHeader AS
+CREATE OR ALTER VIEW [analytics].[OrgHeader] AS
 WITH OrgHeader AS (
     SELECT
-    [OH_IsValid]
+      [OH_IsValid]
+      ,[OH_PK]
       ,[OH_Code]
       ,[OH_IsActive]
       ,[OH_FullName]
@@ -86,3 +87,4 @@ WITH OrgHeader AS (
     FROM [cleaned].[OrgHeader]
 )
 SELECT * FROM OrgHeader;
+GO
